aa96fc8e13f7eb53604b4612ad29414dcfe37343
Amanda Nelson <amandaln4311@gmail.com>
2021-02-09 11:06:01 -0500

    tv show search by streaming services

--- models/tv.js
+++ models/tv.js
@@ -16,7 +16,7 @@ module.exports = function(sequelize, DataTypes) {
       Hulu: {
         type: DataTypes.BOOLEAN,
       },
-      Primes: {
+      Prime: {
         type: DataTypes.BOOLEAN,
       },
       Disney: {
--- public/js/members.js
+++ public/js/members.js
@@ -10,14 +10,27 @@ $(document).ready(function() {
 
   searchBtnTv.on("click", function(event){
     event.preventDefault();
-    let UserInput = $("input#tvsearch").val().trim();
-
-    $.get("api/" + UserInput, function(data){
+    let userInput = $("input#tvsearch").val().trim();
+    console.log(userInput);
+  
+    
+    $.get(`/api/${userInput}`, function(data){
       console.log(data);
       $("#tvInfo").append("<h4>Title: " + data[0].title + "<h4>");
       $("#tvInfo").append("<h4>Year: " + data[0].year + "<h4>");
+    });
+
+    $.get(`/api/${userInput}/1`, function(data){
+      console.log(data);
+      for (let i = 0; i < data.length; i++) {
+        $("#tvInfo").append("<h3>Title: " + data[i].title + "<h3>");
+        $("#tvInfo").append("<h4>Year: " + data[i].year + "<h4>");
+        
+      }
       
     });
+
+   
   });
 
 
